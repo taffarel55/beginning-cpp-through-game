d5e0ada0673a1c4990e8e87a8f52cf609a81dd39
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-09 11:03:58 -0300

    Questão 2

---
+++ Chapter 1/Exercices/2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+// 2. What’s displayed by each line in the following code snippet? Explain each result. 
+
+int main() {
+    cout << "Seven divided by three is " << 7 / 3 << endl; 
+    cout << "Seven divided by three is " << 7.0 / 3 << endl; 
+    cout << "Seven divided by three is " << 7.0 / 3.0 << endl;
+    return 0;
+}
