a28a63baf1aeac37c026dd07817c9c801a8ca3fc
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-09 11:03:34 -0300

    Questão 1

---
+++ Chapter 1/Exercices/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+    int SCOREOFGAMELEVEL1;
+    int score;
+
+    char MY_Choice_Level;
+    char choiceLevel;
+
+    double $123;
+    double x;
+    
+    return 0;
+}
