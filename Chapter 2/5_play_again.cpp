bd7475f2912e166a580734f95111a7b7b95f8d9e
taffarel55 <mauruciotaffarel@gmail.com>
2021-07-15 14:10:00 -0300

    // Demonstrates while loops

---
+++ Chapter 2/5_play_again.cpp
@@ -0,0 +1,15 @@
+// Play Again 
+// Demonstrates while loops 
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    char again = 'y'; 
+    while (again == 'y') { 
+        cout << "\n**Played an exciting game**"; 
+        cout << "\nDo you want to play again? (y/n): "; 
+        cin >> again; 
+    } 
+    cout << "\nOkay, bye."; 
+    return 0; 
+}
